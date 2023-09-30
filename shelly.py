32974ef09d1a3213882f7afb119b482406696e66
Jean Viaunel <viocitovictor35@gmail.com>
2023-09-30 00:58:45 +0000

    Shelly2023

---
+++ shelly.py
@@ -0,0 +1,9 @@
+def ShellyLator(num1,num2):
+	return num1 + num2
+
+
+num1 = 99
+
+num2 = int(input(f"num1 is {num1}\nEnter num2\n"))
+#Print the result of the two numbers
+print(ShellyLator(num1,num2))
